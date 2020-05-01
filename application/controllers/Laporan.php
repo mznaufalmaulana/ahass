cd02bd8850800ace10654100fd6ef66eb269c7d0
MZ. Naufal Maulana <mznaufalmaulana@gmail.com>
2020-05-01 19:44:06 +0700

    penjagaan login

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -3,6 +3,18 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Laporan extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->helper('form');
+        $this->load->helper('url');
+
+        if (!isset($_SESSION['id'])) {
+            redirect('Auth/');
+        }
+    }
+
     public function index()
     {
         $data['judul'] = "Laporan Penjualan";
